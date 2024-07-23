2135c0bb1262c961a8f7244de0f1668a6030bedd
Ivan Uresti <iuresti@skytouchtechnology.com>
2024-07-22 20:04:30 -0600

    Add login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TherapiesComponent} from "./component/therapies/therapies.component";
+import {LoginComponent} from "./component/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TherapiesComponent],
+  imports: [RouterOutlet, TherapiesComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/services/therapy.service.ts
+++ src/app/services/therapy.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Therapy} from "../classes/therapy";
+import {ConfigurationService} from "./configuration.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TherapyService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private configuration: ConfigurationService) {
+  }
 
-  getAll() : Observable<Therapy[]> {
-    return this.httpClient.get<Therapy[]>("http://localhost:8080/api/therapy");
+  getAll(): Observable<Therapy[]> {
+    return this.httpClient.get<Therapy[]>(`${this.configuration.serverURL()}/api/therapy`)
   }
 }
